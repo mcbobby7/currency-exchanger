b79f2694c46f9fde848f93be558e95dcf1999bb0
mcbobby madu <madumcbobby@yahoo.com>
2022-11-19 12:34:16 +0100

    set up react router and styled components

---
+++ src/Pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Home } from './style'
+
+function App() {
+  return (
+    <Home>
+      <div>ntn new for now</div>
+    </Home>
+  );
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Home from './Pages/Home';
 import reportWebVitals from './reportWebVitals';
 import {
   createBrowserRouter,
@@ -12,7 +12,7 @@ import {
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <Home />,
   },
 ]);
 
